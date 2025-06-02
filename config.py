d01f535742bf50a82f9bb636f856d994f140011b
KrishnaVamsibakka <bakkakrishnavamsireddy09@gmail.com>
2025-06-02 19:42:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only store files of 𝐕𝐚𝐦𝐬𝐢 🐦‍🔥 !"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI am File Store bot Created by 𝐕𝐚𝐦𝐬𝐢 🐦‍🔥 and he can only use me.")
+START_MSG = os.environ.get("START_MESSAGE", "𝓗𝓮𝓵𝓵𝓸 {mention}\n\n𝓘 𝓪𝓶 𝓕𝓲𝓵𝓮 𝓢𝓽𝓸𝓻𝓮 𝓫𝓸𝓽 𝓒𝓻𝓮𝓪𝓽𝓮𝓭 𝓫𝔂 𝐕𝐚𝐦𝐬𝐢 🐦‍🔥 𝓪𝓷𝓭 𝓱𝓮 𝓬𝓪𝓷 𝓸𝓷𝓵𝔂 𝓾𝓼𝓮 𝓶𝓮.")
 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
 
